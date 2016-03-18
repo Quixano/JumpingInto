322ed8e049be12dce23a700f2f084773397f456a
Paul Hirate Wyatt <pahirate@gmail.com>
2016-03-17 18:56:58 -0700

    Create 3-1.cpp

---
+++ 3-1.cpp
@@ -0,0 +1,22 @@
+
+/***********
+Exercise 3.1
+***********/
+//Just concatenating multiple strings.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+   string firstName;
+   string lastName;
+
+   cout << "What is your first name?" << endl;
+   cin  >> firstName;
+   cout << "What is your last name?"  << endl;
+   cin  >> lastName;
+
+   cout << "Your full name is " firstName + " " + lastName ", right?"
+
+}

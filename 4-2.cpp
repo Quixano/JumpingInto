de1c0e4739d00407ff4ab949ad99525fc7397713
Paul Hirate Wyatt <pahirate@gmail.com>
2016-03-17 19:03:10 -0700

    Create 4-2.cpp

---
+++ 4-2.cpp
@@ -0,0 +1,18 @@
+//Practicar 4.2
+//A very insecure "password checker"
+#include <iostream>
+using namespace std;
+int main () {
+int password;
+
+cout << "Please input your password" << endl;
+cin  >> password;
+
+if (password = 1234 || password = 1379)
+  {
+    cout << "Welcome back";
+  }
+  else {
+    cout << "Access Denied!";
+  }
+}

4a737f86fc3bb0450a0e27a7cffb4503a238f3a2
Paul Hirate Wyatt <pahirate@gmail.com>
2016-03-17 18:58:09 -0700

    Create 3-3.cpp

---
+++ 3-3.cpp
@@ -0,0 +1,24 @@
+
+/***********
+Exercise 3.3
+***********/
+//Concatenating strings and integer values
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   int firstNumber;
+   int secondNumber;
+   cout << "What is the first number (a): " << endl;
+   cin  >> firstNumber;
+   cout << "What is the second number (b): "<< endl;
+   cin  >> secondNumber;
+
+   cout << "Dividing these values("<<firstNumber<<"/"<<secondNumber<<"returns"<<firstNumber/secondNumber<<endl;
+
+   return 0;
+}
